f289c91e938411acd0291827e573068fa9060eaa
Sarath T S <sarathiscookie@hotmail.com>
2023-10-22 23:00:03 +0000

    Frontend Project: Changed variable name, clean up, added sudo, updated conditions

--- frontend_con.sh
+++ frontend_con.sh
@@ -9,69 +9,78 @@ exec &> "$log_file"
 # Read .env file and fill variables.
 company_dir=$(cat .env | grep COMPANY_DIR | awk -F'=' '{print $2}')
 project_dir=$(cat .env | grep PROJECT_DIR | awk -F'=' '{print $2}')
-user_name=$(cat .env | grep USER | awk -F'=' '{print $2}')
-group_name=$(cat .env | grep GROUP | awk -F'=' '{print $2}')
+user=$(cat .env | grep USER | awk -F'=' '{print $2}')
+group=$(cat .env | grep GROUP | awk -F'=' '{print $2}')
 
 # Create user
-create_user_name() {
-    useradd $user_name
+create_user() {
+    sudo useradd $user
 
-    if [ $? -eq 0 ]
+    if [ $? == 0 ]
         then 
-            echo "### User $user_name created successfully. ###"
+            echo "### User $user created successfully. ###"
     fi                         
 }
 
 # Create group
-create_group_name() {
-    groupadd $group_name
+create_group() {
+    sudo groupadd $group
 
-    if [ $? -eq 0 ]
+    if [ $? == 0 ]
         then 
-            echo "Group $group_name created successfully."
+            echo "### Group $group created successfully. ###"
     fi
 }
 
 # Add user to group
 add_user_to_group() {
-    usermod -aG $group_name $user_name
+    sudo usermod -aG $group $user
+    if [ $? == 0 ]
+        then 
+            echo "### User $user added to Group $group. ###"
+    fi
 }
 
-echo $(create_user_name)
-echo $(create_group_name)
+echo $(create_user)
+echo $(create_group)
 echo $(add_user_to_group)
 
 #: '
-hostname=$(cat /etc/hostname)
-if [ $hostname -eq 0 ]
+cat /etc/hostname
+if [ $? == 0 ]
     then
         # Checking project directory.
         ls -ld /usr/local/$company_dir/$project_dir
 
-        if [ $? -eq 0 ]
+        if [ $? == 0 ]
             then 
-                echo "### Found project directory. Exiting... ###"
+                echo "### Project directory already existing ###"
                 exit
         else
-            echo "### Creating project directory. ###"
-            echo
             sudo mkdir -p /usr/local/$company_dir/$project_dir
-            if [ $? -eq 0 ]
+            if [ $? == 0 ]
                 then
                     project_dir_path=$(ls -ld /usr/local/$company_dir/$project_dir)
                     echo "### Directory created. Path: $project_dir_path. ###"
                     echo
 
                     # Add user and group to directory
-                    chown -R $user_name:$group_name /usr/local/$company_dir
-                    echo "### Added USER $user and GROUP $group ###"
+                    sudo chown -R $user:$group /usr/local/$company_dir
 
-                    # If dir then pass the branch name, clone project from repo, .
-                    # Check status of the clone using if condition. 
-                    # If successfull then
+                    if [ $? == 0 ] 
+                        then
+                            echo "### Added USER $user and GROUP $group ###"
+
+                            # clone project from repo.
+                    else
+                        echo "### Failed: USER $user and GROUP $group not added. ###"        
+                    fi 
             fi
         fi            
 else
     echo "Hostname not found!"        
 fi
 #'
+
+
+#[root@vm01 local]# groupdel devgroup; userdel devadmin; rm -rf /home/devadmin; rm -rf company
